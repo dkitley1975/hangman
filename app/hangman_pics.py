0a05bf903fff009235456ce0825c06f9ca21625b
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-11-05 09:25:59 +0000

    Add: hangman stages function and test individually

---
+++ app/hangman_pics.py
@@ -0,0 +1,100 @@
+def display_hangman(tries):
+    """
+    Images to represent the different stages in the hanging Hangman images
+    initially source from a youtube tutorial added additional stages
+    and amended the graphics to my reguirements.
+    """
+
+    stages = [
+        # final state: hunghead, torso, both arms, and both legs
+        """
+                                    --------
+                                    |      |
+                                    |     [x]
+                                    |     \\|/
+                                    |      |
+                                    |     / \\
+                                    --------
+        """,
+        # head, torso, both arms, and both legs
+        """
+                                    --------
+                                    |      |
+                                    |      O
+                                    |     \\|/
+                                    |      |
+                                    |     / \\
+                                    --------
+        """,
+        # head, torso, both arms, and one leg
+        """
+                                    --------
+                                    |      |
+                                    |      O
+                                    |     \\|/
+                                    |      |
+                                    |     /
+                                    --------
+        """,
+        # head, torso, and both arms
+        """
+                                    --------
+                                    |      |
+                                    |      O
+                                    |     \\|/
+                                    |      |
+                                    |
+                                    --------
+        """,
+        # head, torso, and one arm
+        """
+                                    --------
+                                    |      |
+                                    |      O
+                                    |     \\|
+                                    |      |
+                                    |
+                                    --------
+        """,
+        # head and torso
+        """
+                                    --------
+                                    |      |
+                                    |      O
+                                    |      |
+                                    |      |
+                                    |
+                                    --------
+        """,
+        # head
+        """
+                                    --------
+                                    |      |
+                                    |      O
+                                    |
+                                    |
+                                    |
+                                    --------
+        """,
+        # initial empty state, with man beside
+        """
+                                    --------
+                                    |      |
+                                    |
+                                    |
+                                    |
+                                    |
+                                    --------
+        """,
+            # initial empty state
+        """
+                                    --------
+                                    |
+                                    |
+                                    |
+                                    |
+                                    |
+                                    --------
+        """,
+    ]
+    return stages[tries]
