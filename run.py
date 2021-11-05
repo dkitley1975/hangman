9b69c35269e0793a241d473cfccd1d94063174d8
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-11-05 09:29:14 +0000

    Add: menu to the run.py

--- run.py
+++ run.py
@@ -1,3 +1,60 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import sys
+
+from app.clear_function import clear_terminal
+from app.ascii_art import hangman_ascii_welcome
+from app.ascii_art import hangman_ascii_bye
+from app.ascii_art import hangman_ascii_lets_play
+from app.ascii_art import hangman_ascii_hangman
+from app.get_new_word import get_word
+from app.play_game import play
+from app.high_scores import display_highscores
+from app.rules import display_rules
+
+def welcome():
+    """
+    Displays the welcome message.
+    """
+    hangman_ascii_welcome()
+
+def menu():
+    """
+    Displays the navigation to start a game, view high scores, view rules or quit.
+    """
+    clear_terminal()
+    print('\n' * 3)
+    print(f"{'  Main Menu ! ':*^80}")
+    print('\n' * 2)
+    print(f"{' 1: Play Hangman ':^80}")
+    print('\n' * 1)
+    print(f"{' 2: View High Scores ':^80}")
+    print('\n' * 1)
+    print(f"{' 3: Rules ':^80}")
+    print('\n' * 1)
+    print(f"{' 4: QUIT ':^80}")
+    print('\n' * 2)
+    while True:
+        player_menu_selection = input(' ' * 21 + ' Please make a choice (1, 2, 3 or 4): ')
+        if player_menu_selection == '1':
+            hangman_ascii_lets_play()
+            hangman_ascii_hangman()
+            print("You choose 1")
+            while input(' ' * 20 + "Would you like to play again? (Y/N) ").upper() == "Y":
+                print("You choose 1")
+            menu()
+        elif player_menu_selection == '2':
+            display_highscores()
+            menu()
+        elif player_menu_selection == '3':
+            display_rules()
+            menu()
+        elif player_menu_selection == '4':
+            hangman_ascii_bye()
+            sys.exit()
+        else:
+            print(f"{' Must choose 1, 2, 3 or 4 ! ':^80}")
+
+welcome()
+menu()
