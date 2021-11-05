8074a3b7e90560b8407fb6d4b055cb341f1a6ceb
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-11-05 09:25:10 +0000

    Add: get new word function and test

---
+++ app/get_new_word.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_word():
+    """
+    requests a single random word form the hangman.api
+    and converts it to uppercase before returning the word
+    """
+    url = "https://random-word-api.herokuapp.com/word?number=1"
+
+    response = requests.request("GET", url)
+    answer = response.json()
+    return answer[0].upper()
+    
