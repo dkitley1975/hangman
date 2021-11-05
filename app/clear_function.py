10098050a93180d05c9feb0985f99bb16d92d4aa
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-11-05 09:24:15 +0000

    Add: clear terminal function

---
+++ app/clear_function.py
@@ -0,0 +1,11 @@
+import os
+from time import sleep
+
+
+# define clear function - information initally searched from Stackoverflow
+def clear_terminal():
+    """
+    Clears the terminal screen after 1 second
+    """
+    sleep(1)
+    os.system('cls' if os.name=='nt' else 'clear')
