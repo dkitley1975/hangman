861ffe86de8c81ba8944cca35bc46142e42f9924
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-11-05 16:56:09 +0000

    Bugfix: when word guessed , game stopped ending

--- app/play_game.py
+++ app/play_game.py
@@ -30,15 +30,15 @@ def play(word):
     print(display_hangman(tries) + nl_1)
     print(f"{word_completion:^80}{nl_1}")
     while not guessed and tries > 0:
-        guess = input(' ' * 25 + "Please guess a letter or word: \n" +
-                      ' ' * 40).upper()
+        guess = input(' ' * 23 + "Please guess a letter or word: ").upper()
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print('{:^80}'.format("You have already guessed " + guess +
                                       " please try another letter"))
                 sleep(1)
             elif guess not in word:
-                print(f"{'Sorry, ' + guess + ', is not the word.':^80}")
+                print('{:^80}'.format("Sorry " + guess + " is not in the word"
+                                      " please try another letter"))
                 sleep(1)
                 tries -= 1
                 guessed_letters.append(guess)
@@ -51,7 +51,7 @@ def play(word):
                 for index in indices:
                     word_as_list[index] = guess
                 word_completion = "".join(word_as_list)
-                if "" not in word_completion:
+                if "_" not in word_completion:
                     guessed = True
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
@@ -59,8 +59,8 @@ def play(word):
                                       " please try another letter"))
                 sleep(1)
             elif guess != word:
-                print(f"{'Sorry, ' + guess + ', is not the word.':^80}")
-                sleep(1)
+                print('{:^80}'.format("Sorry " + guess +
+                                      " is not the word please try again"))
                 sleep(1)
                 tries -= 1
                 guessed_words.append(guess)
@@ -70,23 +70,23 @@ def play(word):
         else:
             print("Not a valid guess.")
         clear_terminal()
-        print(nl_1)
-        print('{:^80}'.format("there are " + str(total_letters) +
-                              " letters in the word") + nl_1)
-        guessed_letters_string = ",".join(guessed_letters)
+        guessed_letters_string = ", ".join(guessed_letters)
         print(f"{'Your previously guesses letters are:':^80}")
         print(f"{guessed_letters_string :^80}")
         print(display_hangman(tries))
-        print(f"{word_completion:^80}{nl_2}")
+        print(f'{word_completion:^80}')
+        print('{:^80}'.format("there are " + str(total_letters) +
+                              " letters in the word") + nl_1)
     if guessed:
         new_score = []
         new_score.extend((tries, player_name, word.title()))
         update_highscores(new_score)
         hangman_ascii_congratulations()
-        print(f"{'Congratulations ' + player_name + '.':^80}")
-        print(f"{'You guessed the word ' + word + ', correctly.':^80}")
+        print('\n' * 9)
+        print(f"{'Congratulations ' + player_name + '.':^80}" + nl_1)
+        print(f"{'You guessed the word ' + word + ', correctly.':^80}" + nl_1)
     else:
         hangman_ascii_die()
-        print('\n' * 12)
-        print(f"{player_name + ', you ran out of tries.':^80}")
-        print(f"{'The word was ' + word + '.':^80}{nl_1}")
+        print('\n' * 9)
+        print(f"{player_name + ', you ran out of tries.':^80}" + nl_1)
+        print(f"{'The word was ' + word + '.':^80}" + nl_1)
